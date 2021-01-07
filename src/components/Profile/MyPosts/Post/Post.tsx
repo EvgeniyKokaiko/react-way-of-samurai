c7e336a5ba9bc88d9fdb63060d5e7a164de708a5
EvgeniyKokaiko <70027656+EvgeniyKokaiko@users.noreply.github.com>
2021-01-07 18:20:52 +0200

    увууву
    увув

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:src/components/Profile/MyPosts/Post/Post.tsx
 import React from 'react';
 import s from './Post.module.css';
 
@@ -19,3 +20,22 @@ const Post: React.FC<PropsType> = (props) => {
 }
 
 export default Post;
+=======
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = (props) => {
+
+  return (
+    <div className={s.item}>
+      <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' />
+        { props.message }
+          <div>
+        <span>like</span> { props.likesCount }
+      </div>
+    </div>
+  )
+}
+
+export default Post;
+>>>>>>> parent of 549e262... 26: dialogs structure refactoring; UI-BLL:src/components/Profile/MyPosts/Post/Post.jsx
